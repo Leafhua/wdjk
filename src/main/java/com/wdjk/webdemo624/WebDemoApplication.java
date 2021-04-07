1ac2528a80108734fd51cd0edad11f9fd2803103
zhuhua <1007664276@qq.com>
2021-04-07 18:44:19 +0800

    无敌女高中生留言板初版

---
+++ src/main/java/com/wdjk/webdemo624/WebDemoApplication.java
@@ -0,0 +1,18 @@
+package com.wdjk.webdemo624;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+*@author zhuhua
+
+
+ */
+@SpringBootApplication
+public class WebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdjk/webdemo624/WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdjk.webdemo624;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
