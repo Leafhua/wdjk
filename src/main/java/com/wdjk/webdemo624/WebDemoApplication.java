0e2dd0c87abc859b8084013a23cdfa554fcbf9e5
zhuhua <1007664276@qq.com>
2021-05-04 15:57:27 +0800

    加入扫描mapper注解

--- src/main/java/com/wdjk/webdemo624/WebDemoApplication.java
+++ src/main/java/com/wdjk/webdemo624/WebDemoApplication.java
@@ -1,5 +1,6 @@
 package com.wdjk.webdemo624;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
  */
+
 @SpringBootApplication
+@MapperScan("com.wdjk.webdemo624.mapper")
 public class WebDemoApplication {
 
 	public static void main(String[] args) {
