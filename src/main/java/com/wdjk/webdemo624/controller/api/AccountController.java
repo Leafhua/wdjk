86e090c22c83a439b9c1bc5acd0f347e0dea9cae
zhuhua <1007664276@qq.com>
2021-05-31 02:42:51 +0800

    完成注销，查询登录用户信息，修改邮箱操作

--- src/main/java/com/wdjk/webdemo624/controller/api/AccountController.java
+++ src/main/java/com/wdjk/webdemo624/controller/api/AccountController.java
@@ -1,21 +1,15 @@
 package com.wdjk.webdemo624.controller.api;
 
-import com.mysql.cj.util.StringUtils;
 import com.wdjk.webdemo624.constant.api.ParamConst;
+import com.wdjk.webdemo624.constant.api.SetConst;
+import com.wdjk.webdemo624.controller.annotation.LoginAuthorization;
 import com.wdjk.webdemo624.dto.ApiJsonDTO;
 import com.wdjk.webdemo624.entity.User;
-import com.wdjk.webdemo624.mapper.UserMapper;
-import com.wdjk.webdemo624.service.UserService;
-import com.wdjk.webdemo624.utils.RandomUtil;
-import io.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import com.wdjk.webdemo624.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.TemplateEngine;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -32,54 +26,29 @@ import java.util.Map;
 @RequestMapping(value = "/api/account")
 public class AccountController {
 
-    @Resource
-    private  UserService userService;
+    private final UserService userService;
+    private final IValidationService validationService;
+    private final ISecretService secretService;
+    private final ICacheService cacheService;
+    private final IFtpService ftpService;
+    private final IHttpService httpService;
+    private final IEmailService emailService;
+    private final ICaptchaService captchaService;
+    private final IRandomService randomService;
 
-    @Resource
-    private TemplateEngine templateEngine;
-
-    @Resource
-    private UserMapper userMapper;
-
-    @Resource
-    private StringRedisTemplate stringRedisTemplate;
-
-    @Value("${wdjk.path.domain}")
-    private String domain;
-
-    @Value("${server.servlet.context-path}")
-    private String contextPath;
-
-    /**
-     * 登录
-     *
-     *
-     * @param username  用户名
-     * @param password  密码
-     * @return
-     */
-
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String login(@ApiParam(name = "name") @RequestParam("name") String username,
-                        @ApiParam("密码") @RequestParam("password")String password){
-
-        if(!StringUtils.isNullOrEmpty(username)&&"123456".equals(password)){
-        return "OK";}
-        else{
-            return "登录失败";
-        }
+    @Autowired
+    private AccountController(UserService userService, IValidationService validationService, ISecretService secretService, ICacheService cacheService, IFtpService ftpService, IHttpService httpService, IEmailService emailService, ICaptchaService captchaService, IRandomService randomService) {
+        this.userService = userService;
+        this.validationService = validationService;
+        this.secretService = secretService;
+        this.cacheService = cacheService;
+        this.ftpService = ftpService;
+        this.httpService = httpService;
+        this.emailService = emailService;
+        this.captchaService = captchaService;
+        this.randomService = randomService;
     }
 
-    @RequestMapping(path = "/login",method = RequestMethod.POST)
-    public String login(String username,
-                        String password,
-                        String code,
-                        String rememberMe,
-                        HttpSession session,
-                        HttpServletResponse response){
-
-        return null;
-    }
 
     /**
      * 注册
@@ -92,8 +61,83 @@ public class AccountController {
         String username = (String) requestBodyParamsMap.get(ParamConst.USERNAME);
         String password = (String) requestBodyParamsMap.get(ParamConst.PASSWORD);
         String email = (String) requestBodyParamsMap.get(ParamConst.EMAIL);
-        String code = RandomUtil.generateRandomString(6);
         User user = userService.register(username,password,email);
         return new ApiJsonDTO().success().model(userService.getUserInfoModelMap(user));
     }
+
+
+    /**
+     * 登录
+     *      - username（支持用户名 or 邮箱格式）
+     *
+     * @param requestBodyParamsMap  request-body 内 JSON 数据
+     * @return ApiJsonDTO 接口 JSON 传输对象
+     */
+    @RequestMapping(path = "/login",method = RequestMethod.POST)
+    public ApiJsonDTO loginAccount(@RequestBody Map<String,Object> requestBodyParamsMap){
+        String username = (String) requestBodyParamsMap.get(ParamConst.USERNAME);
+        String password = (String) requestBodyParamsMap.get(ParamConst.PASSWORD);
+
+        validationService.checkUsername(username)
+                .check(ParamConst.PASSWORD,password);
+
+        User user = userService.loginVerification(username,password);
+
+        String authentication = secretService.generateUserInfoAuthentication(user);
+        httpService.saveAuthenticationCookie(authentication);
+
+
+        Map<String,Object> modelMap = new LinkedHashMap<>(SetConst.LENGTH_TWO);
+        modelMap.put(ParamConst.AUTHENTICATION,authentication);
+        modelMap.put(ParamConst.STATE,userService.isUserActivatedByState(user.getFuState()));
+
+        return new ApiJsonDTO().success().model(modelMap);
+    }
+
+
+    /**
+     * 注销
+     *
+     * @return ApiJsonDTO 接口 JSON 传输对象
+     */
+    @LoginAuthorization
+    @RequestMapping(value = "/logout",method = RequestMethod.GET)
+    public ApiJsonDTO logoutAccount(){
+        httpService.removeCookie(ParamConst.AUTHENTICATION);
+
+        return new ApiJsonDTO().success();
+    }
+
+    /**
+     * 获取用户信息
+     *
+     *
+     * @return ApiJsonDTO 接口 JSON 传输对象
+     */
+    @LoginAuthorization
+    @RequestMapping(value = "/info",method = RequestMethod.GET)
+    public ApiJsonDTO userInfo(){
+        User user =  secretService.getUserInfoByAuthentication(httpService.getAuthenticationCookieValue());
+        return new ApiJsonDTO().success().model(user);
+    }
+
+    @LoginAuthorization
+    @RequestMapping(value = "/update-email",method = RequestMethod.POST,consumes = "application/json")
+    public ApiJsonDTO updateUserEmail(@RequestBody Map<String,Object> requestBodyMap){
+        String username = (String) requestBodyMap.get(ParamConst.USERNAME);
+        String email = (String) requestBodyMap.get(ParamConst.EMAIL);
+
+        validationService.check(ParamConst.USERNAME,username)
+                .check(ParamConst.EMAIL,email);
+
+        User cookieUser = secretService.getUserInfoByAuthentication(httpService.getAuthenticationCookieValue());
+
+        userService.confirmUserMatchCookieUser(username,cookieUser);
+
+        userService.alterUserEmail(username,email);
+        return new ApiJsonDTO().success();
+
+    }
+
+
 }
