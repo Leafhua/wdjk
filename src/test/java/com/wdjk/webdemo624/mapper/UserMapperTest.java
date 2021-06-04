b3a3a0037dbf93ab5b1fb4aa4090b6088a9cc7f7
zhuhua <1007664276@qq.com>
2021-06-04 16:55:42 +0800

    细节操作

--- src/test/java/com/wdjk/webdemo624/mapper/UserMapperTest.java
+++ src/test/java/com/wdjk/webdemo624/mapper/UserMapperTest.java
@@ -1,11 +1,16 @@
 package com.wdjk.webdemo624.mapper;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wdjk.webdemo624.entity.User;
+import com.wdjk.webdemo624.service.TopicService;
+import com.wdjk.webdemo624.utils.JsonUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
@@ -13,14 +18,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserMapperTest {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private TopicMapper topicMapper;
+    @Autowired
+    private TopicService topicService;
 
     @Test
     public void testSelectUser(){
-        User user = userMapper.selectById(1);
+        User user = userMapper.selectById(59);
         System.out.println(user);
 
-        user = userMapper.selectById(3);
-        System.out.println(user);
+        System.out.println(user.getFuId());
+        System.out.println(user.getFuCreatetime());
+        Map<String,Object> map = JsonUtil.toMapByObject(user);
+        System.out.println(map);
     }
     @Test
     public void testInsertUser(){
@@ -33,7 +44,22 @@ public class UserMapperTest {
         int result = userMapper.insert(user);
         System.out.println(result);
         System.out.println(user.getFuId());
+        System.out.println(user.getFuCreatetime());
+
     }
+    @Test
+    public void testList(){
+        System.out.println(topicService.count());
+    }
+
+    @Test
+    public void testPage(){
+        Page<User> userPage = new Page<>();
+        userPage.setCurrent(3);
+
+    }
+
+
 
 
 }
---
+++ src/test/java/com/wdjk/webdemo624/service/impl/HttpServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.wdjk.webdemo624.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpServiceImplTest {
+
+
+    @Resource
+    private HttpServiceImpl httpService;
+    @Test
+    public void testHttp(){
+
+    }
+
+}
