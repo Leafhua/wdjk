3c01328b945e9fb6a1ceab047e310ea3b9a273b1
zhuhua <1007664276@qq.com>
2021-06-04 16:49:44 +0800

    mybatis-plus自动分页配置

---
+++ src/main/java/com/wdjk/webdemo624/config/MybatisPlusConfig.java
@@ -0,0 +1,30 @@
+package com.wdjk.webdemo624.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @program: webDemo
+ * @description
+ * @author: zhuhua
+ * @create: 2021-06-03 00:56
+ **/
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.wdjk.webdemo624.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+}
